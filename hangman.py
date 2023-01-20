8c6cebbdb5490534cc412cbf8398244d0dc8f06e
connorw <connorw@Connor-PC.localdomain>
2023-01-19 17:46:37 -0700

    adds wordlist pull down

--- hangman.py
+++ hangman.py
@@ -1,2 +1,17 @@
+from urllib.request import urlopen
+
+def read_word():
+    link = "http://norvig.com/ngrams/sowpods.txt"
+    f = urlopen(link)
+    myfile = f.read()
+    print(myfile)
+    
+# def get_word():
+#     pass
+
+# def guess_letter():
+#     pass
+
 if __name__ == "__main__":
-    pass
+ read_file = read_word()
+ print(read_file)
