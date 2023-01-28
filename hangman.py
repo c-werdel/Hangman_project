adf62d620aa85ac778ad7bbacae4a68092b02abd
connorw <connorw@Connor-PC.localdomain>
2023-01-27 17:42:53 -0700

    New branch with turn counter

--- hangman.py
+++ hangman.py
@@ -13,27 +13,53 @@ def get_word(myfile):
     return(word_choice)
 
 def letter_checker(word_choice):
+    fail_count = 0
+    fail_limit = 12
     print("Guess That Word")
     word_completion = "_" *len(word_choice) 
-    word_choice = list(word_choice) 
+    # word_choice = list(word_choice) 
     user_line = list(word_completion)
     wrong_guesses = []
     print("Your word is", user_line)
-    
-    while True:
-        print("Wrong guessed letters: " , wrong_guesses) 
+    playing = True
+
+    while playing:
+        print("Letters guessed wrong: " , wrong_guesses,) 
+        print("You have used up", + fail_count, 'guess of', fail_limit, 'total guesses') 
         guess = input("Guess a letter ").upper()
+        
         index_count = 0
+        correct_printout = False
         for letter in word_choice:
             if letter == guess:
-                print("Correct that letter is in the word")
+                correct_printout = True
                 user_line[index_count] = guess
             index_count += 1
+        if correct_printout:
+            print("Correct", guess, "is in the word") 
         print(user_line)
+        if fail_count >= 12:
+            fail_count -= 1
 
-        if guess not in word_choice:
-            print("Sorry that letter is not in the word")
+        if guess not in word_choice: #only wrong guesses will contrbute to the fail count
+            print("Sorry", guess,  "is not in the word")
             wrong_guesses.append(guess)
+            fail_count += 1
+
+        if user_line == word_choice: #if user line matches word_choice
+            print("correct you guessed", user_line, "and the word was", word_choice,)
+
+        if fail_count == fail_limit:
+            print("Sorry you lost the word was" , word_choice)
+        
+    # new_game = input("Do you want to play again? Enter 'y' or 'n' ") #THIS IS A WORK IN PROGRESS SECTION
+
+    # if new_game[0].lower()=='y':
+    #     playing = True
+    #     continue
+    # else:
+    #     print("Thanks for playing")
+    #     break
 
 if __name__ == "__main__":
     myfile = read_words()
