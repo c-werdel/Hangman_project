00a59dfad6a31addf789d750446e1e4f94541c10
connorw <connorw@connorwerdel@gmail.com>
2023-02-07 17:46:37 -0700

    Added game loop/guessed double letter

--- hangman.py
+++ hangman.py
@@ -19,15 +19,20 @@ def letter_checker(word_choice):
     word_completion = "_" *len(word_choice) 
     # word_choice = list(word_choice) 
     user_line = list(word_completion)
-    wrong_guesses = []
-    print("Your word is", user_line)
-    playing = True
-
-    while playing:
-        print("Letters guessed wrong: " , wrong_guesses,) 
+    guessed = []
+    print(word_choice)
+        
+    while True:
+        print("Your word is", user_line)
+        print("Letters guessed wrong: " , guessed,) 
         print("You have used up", + fail_count, 'guess of', fail_limit, 'total guesses') 
         guess = input("Guess a letter ").upper()
         
+        if guess in guessed or guess in user_line:
+            print("you already guessed", guess)
+            continue
+        #
+
         index_count = 0
         correct_printout = False
         for letter in word_choice:
@@ -36,34 +41,39 @@ def letter_checker(word_choice):
                 user_line[index_count] = guess
             index_count += 1
         if correct_printout:
-            print("Correct", guess, "is in the word") 
-        print(user_line)
-        if fail_count >= 12:
-            fail_count -= 1
+            print("Correct", guess, "is in the word")
+            if fail_count > 0:
+                fail_count -= 1 
 
+       
         if guess not in word_choice: #only wrong guesses will contrbute to the fail count
             print("Sorry", guess,  "is not in the word")
-            wrong_guesses.append(guess)
+            guessed.append(guess)
             fail_count += 1
-
-        if user_line == word_choice: #if user line matches word_choice
+    
+        if "".join(user_line) == word_choice: #if user line matches word_choice
             print("correct you guessed", user_line, "and the word was", word_choice,)
-
-        if fail_count == fail_limit:
-            print("Sorry you lost the word was" , word_choice)
+            break
+        #put a list back into a string
         
-    # new_game = input("Do you want to play again? Enter 'y' or 'n' ") #THIS IS A WORK IN PROGRESS SECTION
+        if fail_count >= fail_limit:
+            print("Sorry you lost the word was" , word_choice)
+            break
 
-    # if new_game[0].lower()=='y':
-    #     playing = True
-    #     continue
-    # else:
-    #     print("Thanks for playing")
-    #     break
+    answer = input("Do you want to play again? Y/N ")
+
+    if answer.lower() == "y" or answer.lower() == "yes":
+       game_setup()
+    else:
+        print("Goodbye!")
+
+def game_setup():
+    choice_word = get_word(myfile)
+    letter_checker(choice_word)
 
 if __name__ == "__main__":
     myfile = read_words()
-    choice_word = get_word(myfile)
-    letter_checker(choice_word)
+    game_setup()
+  
 
     
